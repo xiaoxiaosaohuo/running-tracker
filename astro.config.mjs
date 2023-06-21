4adf903ed7a89221723f088d48355f90835f8958
xin.jin <jinxin479@gmail.com>
2023-06-21 17:18:54 +0800

    feat:init repo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,14 @@
 import { defineConfig } from 'astro/config';
-
+import serviceWorker from "astrojs-service-worker";
+import react from "@astrojs/react";
+import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [react(),tailwind(),serviceWorker({ 
+        workbox: { inlineWorkboxRuntime: true }
+       })],
+  image: {
+    // Example: Enable the Sharp-based image service
+    service: { entrypoint: 'astro/assets/services/sharp' },
+  },
+});
